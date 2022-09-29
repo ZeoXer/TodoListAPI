7eb8dac1f44e34078cbad52641bac9ed99010bc5
ZeoXer <andrew17413@gmail.com>
2022-09-29 14:09:28 +0800

    test ignore

---
+++ src/js/all.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./contrtol");
+require("./view");
+require("./TodoListAPI");
+require("../scss/all.scss");
---
+++ src/js/all.ts
@@ -0,0 +1,4 @@
+import "./contrtol";
+import "./view";
+import "./TodoListAPI";
+import "../scss/all.scss";
